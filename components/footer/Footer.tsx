b3c7d83b31be7a754f0a17d0d6f40f32fd62811d
Amal P Mathews <amalpmathews2003@gmail.com>
2024-02-17 17:01:29 +0530

    Merge branch 'main' of https://github.com/FOSSMeetNITCalicut/fossmeet

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,63 +1,78 @@
 import styles from './Footer.module.css'
 
 export default function Footer() {
-  return (
-    <footer className={styles['footer']} id="contact">
-      <div className='outer-margin'>
-        <div className='inner-margin'>
-          <div className={styles['top']}>
-            <div className={styles['left']}>
-              <div className={styles['contact-wrapper']}>
-                <img src='/waving.png' alt='Hello' />
-                <span>Contact</span>
-              </div>
-              <div className={styles['h4-style']}>
-                <p >Get in touch with us</p>
-              </div>
-            </div>
+	return (
+		<footer className={styles['footer']} id='contact'>
+			<div className='outer-margin'>
+				<div className='inner-margin'>
+					<div className={styles['top']}>
+						<div className={styles['left']}>
+							<div className={styles['contact-wrapper']}>
+								<img src='/waving.png' alt='Hello' />
+								<span>Contact</span>
+							</div>
+							<h4>Get in touch with us</h4>
+						</div>
 
-            <div className={styles['right']}>
-              <div className={styles['contact-items-wrapper']}>
-                <div className={styles['contact-item']}>
-                  <div className={styles['contact-item-icon-wrapper']} style={{ backgroundColor: '#F97070' }}>
-                    <img src='/mail.svg' alt='Email' />
-                  </div>
-                  <div className={styles['contact-item-text-wrapper']}>
-                    <span className={styles['contact-item-title']}>Email</span>
-                    <span className={styles['contact-item-value']}>fosscell@nitc.ac.in</span>
-                  </div>
-                </div>
+						<div className={styles['right']}>
+							<div className={styles['contact-items-wrapper']}>
+								<div className={styles['contact-item']}>
+									<div className={styles['contact-item-icon-wrapper']} style={{ backgroundColor: '#F97070' }}>
+										<img src='/mail.svg' alt='Email' />
+									</div>
+									<div className={styles['contact-item-text-wrapper']}>
+										<span className={styles['contact-item-title']}>Email</span>
+										<span className={styles['contact-item-value']}>fosscell@nitc.ac.in</span>
+									</div>
+								</div>
 
-                <div className={styles['contact-item']}>
-                  <div className={styles['contact-item-icon-wrapper']} style={{ backgroundColor: '#7FE0A6' }}>
-                    <img src='/phone.svg' alt='Phone' />
-                  </div>
-                  <div className={styles['contact-item-text-wrapper']}>
-                    <span className={styles['contact-item-title']}>Phone</span>
-                    <span className={styles['contact-item-value']}>+91 12345 67890</span>
-                  </div>
-                </div>
-              </div>
+								<div className={styles['contact-item']}>
+									<div className={styles['contact-item-icon-wrapper']} style={{ backgroundColor: '#7FE0A6' }}>
+										<img src='/phone.svg' alt='Phone' />
+									</div>
+									<div className={styles['contact-item-text-wrapper']}>
+										<span className={styles['contact-item-title']}>Phone</span>
+										<span className={styles['contact-item-value']}>+91 12345 67890</span>
+									</div>
+								</div>
+							</div>
 
-              <div className={styles['social']}>
-                <span className={styles['social-title']}>Social</span>
-                <div className={styles['social-icons-wrapper']}>
-                  <a href="https://telegram.me/foss_meet" target="_blank" rel="noreferrer noopener"><img src="/telegram.svg" alt="Telegram" /></a>
-                  <a href="https://www.instagram.com/fosscellnitc/" target="_blank" rel="noreferrer noopener"><img src="/instagram.svg" alt="Instagram" /></a>
-                  <a href="https://twitter.com/fosscell" target="_blank" rel="noreferrer noopener"><img src="/twitter.svg" alt="Twitter" /></a>
-                  <a href="https://www.mastodon.social/@FOSSCell@floss.social" target="_blank" rel="noreferrer noopener"><img src="/mastadon.svg" alt="Mastodon" /></a>
-                  <a href="https://www.linkedin.com/company/foss-cell-nitc/mycompany/" target="_blank" rel="noreferrer noopener"><img src="/linkedin-black.svg" alt="LinkedIn" /></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={styles['bottom']}>
-        <div className={styles['logo']}>FOSSMeet&apos;24</div>
-        <span>An event conducted by FOSSCell NITC and CSEA</span>
-      </div>
-    </footer >
-
-  )
+							<div className={styles['social']}>
+								<span className={styles['social-title']}>Social</span>
+								<div className={styles['social-icons-wrapper']}>
+									<a href='https://telegram.me/foss_meet' target='_blank' rel='noreferrer noopener'>
+										<img src='/telegram.svg' alt='Telegram' />
+									</a>
+									<a href='https://www.instagram.com/fosscellnitc/' target='_blank' rel='noreferrer noopener'>
+										<img src='/instagram.svg' alt='Instagram' />
+									</a>
+									<a href='https://twitter.com/fosscell' target='_blank' rel='noreferrer noopener'>
+										<img src='/twitter.svg' alt='Twitter' />
+									</a>
+									<a
+										href='https://www.mastodon.social/@FOSSCell@floss.social'
+										target='_blank'
+										rel='noreferrer noopener'
+									>
+										<img src='/mastadon.svg' alt='Mastodon' />
+									</a>
+									<a
+										href='https://www.linkedin.com/company/foss-cell-nitc/mycompany/'
+										target='_blank'
+										rel='noreferrer noopener'
+									>
+										<img src='/linkedin-black.svg' alt='LinkedIn' />
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className={styles['bottom']}>
+				<div className={styles['logo']}>FOSSMeet&apos;24</div>
+				<span>An event conducted by FOSSCell NITC and CSEA</span>
+			</div>
+		</footer>
+	)
 }
--- components/schedule/SchedulePage.tsx
+++ components/schedule/SchedulePage.tsx
@@ -54,50 +54,51 @@ const SchedulePage = ({ schedule }: { schedule: Schedule }) => {
   const infoDay = [day1Arr, day2Arr, day3Arr]
 
   return (
-    <div className="schedulePage h-full font-inter mt-10">
+    <div className="schedulePage h-full font-inter">
       <div
         className="schedulePageHead 
-                            h-36 
-                            bg-neutral-100
-                            flex 
-                            items-center
-                            border-b-2
-                            border-zinc-950
-            "
+                        h-36 
+                        bg-neutral-100
+                        flex 
+                        items-center
+                        border-b-2
+                        border-zinc-950
+        "
       >
         <Image
           src={logo}
           alt={'logo'}
           className="schedulePageHeadLogo
-                                            md:h-[45%]
-                                            h-[45%]
-                                            ml-16
-                                            md:ml-28
-                "
+                                        md:h-[45%]
+                                        h-[45%]
+                                        ml-16
+                                        md:ml-28
+            "
         />
         <div
           className="schedulePageHeadText
-                                pl-4
-                "
+                            pl-4
+            "
         >
           <div
             className="smallText
-                                    text-sm
-                                    font-inter
-                                    text-zinc-600
-                    "
+                                text-sm
+                                font-inter
+                                text-zinc-600
+                "
           >
             EVENT
           </div>
           <div
             className="bigText
-                                    font-inter
-                                    text-4xl
-                                    font-semibold
-                                    md:text-5xl
-                                    
-                    
-                    "
+                                font-inter
+                                text-4xl
+                                font-bold
+                                tracking-tighter
+                                md:text-6xl
+                                
+                
+                "
           >
             Schedule
           </div>
@@ -105,128 +106,128 @@ const SchedulePage = ({ schedule }: { schedule: Schedule }) => {
       </div>
       <div
         className="timeline 
-                            h-4/5
-                            flex flex-col 
-                            items-center 
-            "
+                        h-4/5
+                        flex flex-col 
+                        items-center 
+        "
       >
         <div
           className="timelineButtons
-                                bg-white
-                                h-14
-                                mt-10 
-                                w-11/12 md:w-1/2
-                                border-2 border-black divide-x-2 divide-black
-                                flex
-                                font-inter
-                "
+                            bg-white
+                            h-14
+                            mt-10 
+                            w-11/12 md:w-1/2
+                            border-2 border-black divide-x-2 divide-black
+                            flex
+                            font-inter
+            "
         >
           <div
             className={`btn1
-                                    w-1/3
-                                    flex items-center justify-center
-                                    md:flex-row
-                                    flex-col
-                                    md:gap-2
-                                    
-                                    ${
-                                      tabNo == 0
-                                        ? 'text-white bg-black '
-                                        : 'hover:bg-orange-200'
-                                    }
-                                    
-                    `}
+                                w-1/3
+                                flex items-center justify-center
+                                md:flex-row
+                                flex-col
+                                md:gap-2
+                                
+                                ${
+                                  tabNo == 0
+                                    ? 'text-white bg-black '
+                                    : 'hover:bg-orange-200 cursor-pointer'
+                                }
+                                
+                `}
             onClick={() => setTabno(0)}
           >
             <div
               className="DAY
-                                        font-inter text-zinc-400 text-sm
-                        "
+                                    font-inter text-zinc-400 text-sm
+                    "
             >
               DAY 1
             </div>
             <div
               className="date
-                                         font-inter text-xl 
-                                        
-                        "
+                                     font-inter text-xl 
+                                    
+                    "
             >
-              Mar 15
+              Mar 22
             </div>
           </div>
           <div
             className={`btn2
-                                    w-1/3
-                                    flex items-center justify-center
-                                    md:flex-row
-                                    flex-col
-                                    md:gap-2
-                                    ${
-                                      tabNo == 1
-                                        ? 'text-white bg-black '
-                                        : 'hover:bg-orange-200'
-                                    }
-                                    
-                    `}
+                                w-1/3
+                                flex items-center justify-center
+                                md:flex-row
+                                flex-col
+                                md:gap-2
+                                ${
+                                  tabNo == 1
+                                    ? 'text-white bg-black '
+                                    : 'hover:bg-orange-200 cursor-pointer'
+                                }
+                                
+                `}
             onClick={() => setTabno(1)}
           >
             <div
               className="DAY
-                                        font-inter text-zinc-400 text-sm
-                        "
+                                    font-inter text-zinc-400 text-sm
+                    "
             >
               DAY 2
             </div>
             <div
               className="date
-                                         font-inter text-xl 
-                                        
-                        "
+                                     font-inter text-xl 
+                                    
+                    "
             >
-              Mar 16
+              Mar 23
             </div>
           </div>
           <div
             className={`btn3
-                                    w-[34%]
-                                    flex items-center justify-center
-                                    md:flex-row
-                                    flex-col
-                                    md:gap-2
-                                    ${
-                                      tabNo == 2
-                                        ? 'text-white bg-black '
-                                        : 'hover:bg-orange-200'
-                                    }
-                                    
-                    `}
+                                w-[34%]
+                                flex items-center justify-center
+                                md:flex-row
+                                flex-col
+                                md:gap-2
+                                ${
+                                  tabNo == 2
+                                    ? 'text-white bg-black '
+                                    : 'hover:bg-orange-200 cursor-pointer'
+                                }
+                                
+                `}
             onClick={() => setTabno(2)}
           >
             <div
               className="DAY
-                                        font-inter text-zinc-400 text-sm
-                        "
+                                    font-inter text-zinc-400 text-sm
+                    "
             >
               DAY 3
             </div>
             <div
               className="date
-                                         font-inter text-xl 
-                                        
-                        "
+                                     font-inter text-xl 
+                                    
+                    "
             >
-              Mar 17
+              Mar 24
             </div>
           </div>
         </div>
         <div
           className="timelineMain
-                                md:w-3/4
-                                w-11/12
-                                h-auto
-                                my-8
-                                md:p-10
-                "
+                            md:w-3/4
+                            w-11/12
+                            h-auto
+                            my-8
+                            md:p-10
+            "
         >
           {infoDay[tabNo].map((x, i) => {
             if (x.noImg && i == infoDay[tabNo].length - 1) {
